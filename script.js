ab683a7eb6d5f62bbf6be2e1b37df6918a50a970
victor-bastos <victorhugo.o.bastos@gmail.com>
2024-03-11 21:18:47 -0300

    opa

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ document.addEventListener('DOMContentLoaded', function () {
             this.nextElementSibling.classList.toggle('active');
         });
     });
+
+    // Extrai o ID do Pokémon da URL
+    const urlParams = new URLSearchParams(window.location.search);
+    const pokemonId = urlParams.get('id'); // Supõe que a URL é algo como "pagina.html?id=25"
+
+    // Busca o nome do Pokémon usando o ID
+    if (pokemonId) {
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+            .then(response => response.json())
+            .then(data => {
+                const pokemonName = data.name;
+                // Atualiza o nome do usuário para o nome do Pokémon
+                document.querySelector('header h2').textContent = `Bem-vindo, ${pokemonName}`;
+            })
+            .catch(error => console.error('Falha ao buscar dados do Pokémon:', error));
+    }
 });
