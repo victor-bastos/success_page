388da56a9d241d08bd605ed46505c6bbd4d57f6e
victor-bastos <victorhugo.o.bastos@gmail.com>
2024-03-12 10:05:19 -0300

    Tradutor e tudo

--- script.js
+++ script.js
@@ -13,13 +13,24 @@ document.addEventListener('DOMContentLoaded', function () {
     const pokemonId = urlParams.get('id'); // Supõe que a URL é algo como "pagina.html?id=25"
 
     // Busca o nome do Pokémon usando o ID
+    // Busca o nome e a imagem do Pokémon usando o ID
     if (pokemonId) {
         fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
             .then(response => response.json())
             .then(data => {
                 const pokemonName = data.name;
                 // Atualiza o nome do usuário para o nome do Pokémon
-                document.querySelector('header h2').textContent = `Bem-vindo, ${pokemonName}`;
+                document.querySelector('header h2').textContent = `${pokemonName}`;
+
+                // Extrai a URL da imagem frontal padrão
+                const pokemonImageURL = data.sprites.front_default;
+
+                // Atualiza a imagem do Pokémon na página
+                const pokemonImageElement = document.querySelector('.pokemon-image');
+                if (pokemonImageElement) {
+                    pokemonImageElement.src = pokemonImageURL;
+                    pokemonImageElement.alt = `Imagem de ${pokemonName}`;
+                }
             })
             .catch(error => console.error('Falha ao buscar dados do Pokémon:', error));
     }
